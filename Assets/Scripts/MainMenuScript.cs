dd363fc2defe9b80f161137b541160fb091599ff
Jonathan Dervin <jdervin@ltu.edu>
2021-04-05 13:50:14 -0400

    Added Scenes

---
+++ Assets/Scripts/MainMenuScript.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuScript : MonoBehaviour
+{
+
+    public string startSceneName;
+    public string Menu;
+    public string Credits;
+
+    public AudioSource soundSource;
+    public AudioClip menu;
+
+    public void StartPressed()
+    {
+        SceneManager.LoadScene(startSceneName);
+      
+    
+    
+    }
+    public void MenuPressed()
+    {
+        soundSource.PlayOneShot(menu);
+        SceneManager.LoadScene(Menu);
+
+
+
+    }
+    public void CreditsPressed()
+    {
+        SceneManager.LoadScene(Credits);
+
+
+
+    }
+    public void CloseGame()
+    {
+        Application.Quit();
+
+
+    }
+
+    
+    
+}
